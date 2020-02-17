4f978a3c5787be6f93f4f1ca15e36b5198717fe5
Marielle Gueissaz <marielle.gueissaz@gmail.com>
2020-02-17 17:05:11 +0100

    added fake friends

---
+++ db/seeds.rb
@@ -0,0 +1,49 @@
+Friend.create!(
+  first_name: "Hugo",
+  last_name: "Mori",
+  friendship_category: "Humour",
+  price_p_hour: "40",
+  description: "Very friendly and funny",
+  location: "Mulhouse",
+  user_id: 1
+)
+
+Friend.create!(
+  first_name: "Lea",
+  last_name: "Medrano",
+  friendship_category: "Fête",
+  price_p_hour: "40",
+  description: "Very friendly and funny",
+  location: "Annemasse",
+  user_id: 1
+)
+
+Friend.create!(
+  first_name: "Thibault",
+  last_name: "Jaime",
+  friendship_category: "Sérieux",
+  price_p_hour: "40",
+  description: "Very friendly and funny",
+  location: "Aix-en-Provence",
+  user_id: 1
+)
+
+Friend.create!(
+  first_name: "Sinan",
+  last_name: "Ucak",
+  friendship_category: "Incredible",
+  price_p_hour: "40",
+  description: "Very smart and funny",
+  location: "New York",
+  user_id: 1
+)
+
+Friend.create!(
+  first_name: "Georgina",
+  last_name: "Noten",
+  friendship_category: "Humour",
+  price_p_hour: "40",
+  description: "Very sweet and funny",
+  location: "Chambéry",
+  user_id: 1
+)
