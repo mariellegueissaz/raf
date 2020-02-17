97b2ccf817815a6ef161f32fc2ebfeed393ca36b
Marielle Gueissaz <marielle.gueissaz@gmail.com>
2020-02-17 16:09:54 +0100

    Merging to master, changed error in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookings, :friends, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :friends, dependent: :destroy
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
