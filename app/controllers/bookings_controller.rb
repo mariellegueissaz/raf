ad80261aafb973e9471d124603ac1492b55f3f92
Arthur Messean <arthur.messean@gmail.com>
2020-02-17 17:56:06 +0100

    bookings in progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,22 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
-  def show
-  end
-
-  def new
+   def new
+    @friend = Friend.find(params[:friend_id])
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @friend = Friend.find(params[:friend_id])
+    @booking.friend = @friend
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +26,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def boooking_params
+    params.require(:booking).permit(:start_time, :end_time, :booking_price, :friend_id, :user_id)
   end
 end
