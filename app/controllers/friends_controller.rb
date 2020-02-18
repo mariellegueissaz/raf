1b93fae8de260c91fb254cfef6fdf69140217ab2
Marielle Gueissaz <marielle.gueissaz@gmail.com>
2020-02-18 14:54:28 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,10 +1,16 @@
 class FriendsController < ApplicationController
   def index
+    raise
     @friends = Friend.all
   end
 
   def show
     @friend = Friend.find(params[:id])
+    if @friend.photo.attached?
+      @photo = @friend.photo
+    else
+      @photo = "https://media1.thehungryjpeg.com/thumbs2/ori_3505934_576443aa8ab1896562295dca0561f7550db268c7_adult-guys-men-two-best-friends-friendship-vector-concept.jpgp"
+    end
   end
 
   def new
@@ -13,13 +19,17 @@ class FriendsController < ApplicationController
 
   def create
     @friend = Friend.new(friend_params)
-    @friend.save
+    @friend.user = current_user
+    if @friend.save
+      redirect_to friend_path(@friend)
+    else
+      render :new
+    end
   end
 
   private
 
   def friend_params
-    params.require(:friend).permit(:first_name, :last_name, :friendship_category, :price_p_hour, :description, :location)
-
+    params.require(:friend).permit(:first_name, :last_name, :friendship_category, :price_p_hour, :proho, :description, :location, :user_id)
   end
 end
