657327ff2dfeadacb681846d137aca51a090e108
Marielle Gueissaz <marielle.gueissaz@gmail.com>
2020-02-18 10:11:53 +0100

    changed the root to route to the index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'friends#index'
   resources :friends, only: [:index, :show, :new, :create] do
     resources :bookings
   end
