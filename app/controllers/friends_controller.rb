2d5eb6831b70c7a16ee4b7765561db4da483b52c
Marielle Gueissaz <marielle.gueissaz@gmail.com>
2020-02-18 10:49:31 +0100

    created the friend_params in the friend controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -7,8 +7,18 @@ class FriendsController < ApplicationController
   end
 
   def new
+    @friend = Friend.new
   end
 
   def create
+    @friend = Friend.new(friend_params)
+    @friend.save
+  end
+
+  private
+
+  def friend_params
+    params.require(:friend).permit(:first_name, :last_name, :friendship_category, :price_p_hour, :description, :location)
+
   end
 end
