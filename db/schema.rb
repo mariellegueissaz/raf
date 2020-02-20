7a7c0cb239d8b8883f7f5603120b40e276116cc0
Marielle Gueissaz <marielle.gueissaz@gmail.com>
2020-02-20 16:02:33 +0100

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_02_19_143855) do
-
+ActiveRecord::Schema.define(version: 2020_02_20_140411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +44,6 @@ ActiveRecord::Schema.define(version: 2020_02_19_143855) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "date"
     t.index ["friend_id"], name: "index_bookings_on_friend_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
