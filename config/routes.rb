297d8d4e5c670b5ecf6fbcd235ec38ae3b17509d
Arthur Messean <arthur.messean@gmail.com>
2020-02-20 18:03:39 +0100

    review in progress

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'friends#index'
   devise_for :users
   resources :friends, only: [:index, :show, :new, :create, :edit, :update] do
-    resources :bookings, except: [:index]
+    resources :bookings, except: [:index] do
+      resources :reviews, only: [:index, :show, :new, :create]
   end
+end
   resources :bookings, only: :create
   get 'book_friend', to: 'bookings#book_friend'
   get '/mybookings', to: 'bookings#index'
